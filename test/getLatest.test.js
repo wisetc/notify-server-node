a021398e3aabff0dda447836645098ad1fb595c0
Mark Wang <zhi@uqugu.com>
2020-06-15 14:14:49 +0800

    new test

---
+++ test/getLatest.test.js
@@ -0,0 +1,15 @@
+// @format
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function getLatest() {
+  const url = 'http://localhost:7100/version/latest?platform=wechat';
+  const shellCmd = `curl "${url}"`;
+  console.log(shellCmd);
+  const {stdout, stderr} = await exec(shellCmd);
+  stdout && console.log(stdout);
+  stderr && console.log(stderr);
+  return stdout;
+}
+
+getLatest();
