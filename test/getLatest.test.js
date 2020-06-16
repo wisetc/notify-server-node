b0d7d90549e1dc5d92ea776fbda039e6dd156c2b
Mark Wang <zhi@uqugu.com>
2020-06-16 10:23:30 +0800

    test

--- test/getLatest.test.js
+++ test/getLatest.test.js
@@ -3,7 +3,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 async function getLatest() {
-  const url = 'http://localhost:7100/version/latest?platform=wechat';
+  const url = 'http://localhost:7100/version/latest?platform=dingtalk';
   const shellCmd = `curl "${url}"`;
   console.log(shellCmd);
   const {stdout, stderr} = await exec(shellCmd);
